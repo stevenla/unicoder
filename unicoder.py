639529bf49a90d361485581ddf7c8cdffcccaf16
Steven La <mrstevenla@gmail.com>
2014-01-09 00:32:57 -0800

    fixed the error message

--- unicoder.py
+++ unicoder.py
@@ -3,7 +3,7 @@
 import sys
 
 if len(sys.argv) != 2:
-    print 'usage: % <UNICODE CODE POINT HEX>' % (sys.argv[0])
+    print 'usage: %s <UNICODE CODE POINT HEX>' % (sys.argv[0])
     exit(1)
 
 codepoint = int(sys.argv[1], 16)
