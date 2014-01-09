76dbb816d4ff2aa8f27713b1bb682ffd5a3042b9
Steven La <mrstevenla@gmail.com>
2014-01-08 18:01:35 -0800

    works

---
+++ unicoder.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) != 2:
+    print 'usage: % <UNICODE CODE POINT HEX>' % (sys.argv[0])
+    exit(1)
+
+codepoint = int(sys.argv[1], 16)
+print unichr(codepoint).encode('utf-8')
